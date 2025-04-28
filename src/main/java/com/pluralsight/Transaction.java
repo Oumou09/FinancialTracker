c7e4d0aeaaa097dc38232ab5b7a4aa7c05c2765e
Oumou09 <oumoudesigns@gmail.com>
2025-04-28 10:50:23 -0400

    commit

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,6 +1,12 @@
 package com.pluralsight;
 
 public class Transaction {
+    private int startDate;
+    private int endStart;
+    private String description;
+    private String vendor;
+    private
+
 
 
 }
